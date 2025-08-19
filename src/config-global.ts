8af6edb2e02f9cd221c6d2536de9d557d1464b21
adymar73583418 <adymarr2005@gmail.com>
2025-08-19 12:27:02 +0600

    DEFAULT BASE URL UPDATED

--- src/config-global.ts
+++ src/config-global.ts
@@ -14,7 +14,7 @@ export const CONFIG: ConfigValue = {
   appVersion: packageJson.version,
 };
 
-export const API_BASE_URL = VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+export const API_BASE_URL = VITE_API_BASE_URL || 'http://176.126.164.86:8001';
 export const API_ROUTES = {
   LOGIN: '/api/manager/login/',
 };
